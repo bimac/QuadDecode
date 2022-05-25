8ea9de8819c87ab4f49c4c9a841d2161a8e3f1ad
Florian Rau <bimac@users.noreply.github.com>
2022-05-25 19:17:49 +0200

    added setFTM, formatting

--- src/QuadDecode.cpp
+++ src/QuadDecode.cpp
@@ -1,14 +1,12 @@
 #include "QuadDecode_def.h" // Hardware quadrature encoder
 
-
-QuadDecode<1> *apQDcd1;	    // Pointers to  correct instance for ISR
+QuadDecode<1> *apQDcd1; // Pointers to  correct instance for ISR
 QuadDecode<2> *apQDcd2;
 
-
-void ftm1_isr(void){
+void ftm1_isr(void) { // hello
     apQDcd1->ftm_isr();
 };
 
-void ftm2_isr(void){
+void ftm2_isr(void) {
     apQDcd2->ftm_isr();
 };
